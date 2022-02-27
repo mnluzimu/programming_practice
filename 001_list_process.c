05a083ad36116c1bacc04a823dcb68bdf0fd8451
luzimu <luzimu@mail.ustc.edu.cn>
2022-02-27 08:49:07 +0800

    master

---
+++ 001_list_process.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 5
+#define ERROR -1
+typedef enum {false, true} bool;
+typedef int ElementType;
+typedef int Position;
+typedef struct LNode *List;
+struct LNode {
+    ElementType Data[MAXSIZE];
+    Position Last; /* 保存线性表中最后一个元素的位置 */
+};
+
+List MakeEmpty(); 
+Position Find( List L, ElementType X );
+bool Insert( List L, ElementType X, Position P );
+bool Delete( List L, Position P );
+
+int main()
+{
+    List L;
+    ElementType X;
+    Position P;
+    int N;
+
+    L = MakeEmpty();
+    scanf("%d", &N);
+    while ( N-- ) {
+        scanf("%d", &X);
+        if ( Insert(L, X, 0)==false )
+            printf(" Insertion Error: %d is not in.\n", X);
+    }
+    scanf("%d", &N);
+    while ( N-- ) {
+        scanf("%d", &X);
+        P = Find(L, X);
+        if ( P == ERROR )
+            printf("Finding Error: %d is not in.\n", X);
+        else
+            printf("%d is at position %d.\n", X, P);
+    }
+    scanf("%d", &N);
+    while ( N-- ) {
+        scanf("%d", &P);
+        if ( Delete(L, P)==false )
+            printf(" Deletion Error.\n");
+        if ( Insert(L, 0, P)==false )
+            printf(" Insertion Error: 0 is not in.\n");
+    }
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+
+
+
+
+
+
+List MakeEmpty(){
+    List L;
+    L = (List)malloc(sizeof(struct LNode));
+    L->Last = -1;
+    return L;
+}
+Position Find( List L, ElementType X ){
+    int i;
+    for(i = 0; i <= L->Last; i++){
+        if(L->Data[i] == X)
+            break;
+    }
+    if(i == L->Last + 1)
+        return ERROR;
+    else
+        return i;
+}
+bool Insert( List L, ElementType X, Position P ){
+    if(L->Last + 1 == MAXSIZE){
+        printf("FULL");
+        return false;
+    }
+    if(P > L->Last + 1 || P < 0){
+        printf("ILLEGAL POSITION");
+    }
+    int i;
+    for(i = L->Last; i >= P; i--){
+        L->Data[i + 1] = L->Data[i];
+    }
+    L->Data[P] = X;
+    L->Last++;
+    return true;
+}
+bool Delete( List L, Position P ){
+    if(P > L->Last || P < 0){
+        printf("POSITION %d EMPTY", P);
+        return false;
+    }
+    for(int i = P + 1; i <= P; i++)
+        L->Data[i - 1] = L->Data[i];
+    L->Last--;
+    return true;
+}
